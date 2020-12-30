dd5fc1b35d8849373bcbc3ed2ff8d3c715fd9bf3
izzuddiin <abdullahizzuddiin@gmail.com>
2020-12-30 10:48:28 +0700

    Implement react-hook-form and Yup to register page

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -1,96 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, ScrollView, StyleSheet, ToastAndroid} from 'react-native';
 import {Input, Button, Text} from 'react-native-elements';
+import {Controller, useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import request from './request';
 
+const validationSchema = yup.object({
+    name: yup.string().required('Wajib diisi yup'),
+    email: yup.string().required('Wajib diisi yup'),
+    address: yup.string().required('Wajib diisi yup'),
+    phoneNumber: yup.string().required('Wajib diisi yup'),
+    hobby: yup.string().required('Wajib diisi yup'),
+    favFood: yup.string().required('Wajib diisi yup'),
+    favBeverage: yup.string().required('Wajib diisi yup'),
+    favBook: yup.string().required('Wajib diisi yup'),
+    motto: yup.string().required('Wajib diisi yup'),
+    password: yup.string().required('Wajib diisi yup'),
+    confirmationPassword: yup.string().required('Wajib diisi yup'),
+});
+
 const RegisterPage = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [hobby, setHobby] = useState('');
-    const [favFood, setFavFood] = useState('');
-    const [favBeverage, setFavBeverage] = useState('');
-    const [favBook, setFavBook] = useState('');
-    const [motto, setMotto] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmationPassword, setConfirmationPassword] = useState('');
-    const [error, setError] = useState({});
-
-    const validateForm = () => {
-        const error = {};
-        let anyError = false;
-
-        if (!name) {
-            error.name = 'harus diisi';
-            anyError = true;
-        }
-
-        if (!email) {
-            error.email = 'harus diisi';
-            anyError = true;
-        }
-
-        if (!address) {
-            error.address = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!phoneNumber) {
-            error.phoneNumber = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!hobby) {
-            error.hobby = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!favFood) {
-            error.favFood = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!favBeverage) {
-            error.favBeverage = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!favBook) {
-            error.favBook = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!motto) {
-            error.motto = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!password) {
-            error.password = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (!confirmationPassword) {
-            error.confirmationPassword = 'Harus diisi';
-            anyError = true;
-        }
-
-        if (anyError) {
-            setError(error);
-            throw error;
-        }
-    };
+    const {control, handleSubmit, errors} = useForm({
+        resolver: yupResolver(validationSchema)
+    });
 
     const onSubmit = async () => {
-        try {
-            validateForm();
-            setError({});
-            const response = await request('Register');
-            ToastAndroid.show(response, ToastAndroid.SHORT);
-        } catch (e) {
-            console.log('Ada error');
-        }
+        const response = await request('Register');
+        ToastAndroid.show(response, ToastAndroid.SHORT);
     };
 
     return (
@@ -100,51 +37,117 @@ const RegisterPage = () => {
                 <Text style={styles.subtitle}>Isi form ini dengan data yang benar ya. </Text>
 
                 {/*FORM START*/}
-                <Input
-                    placeholder='Nama'
-                    errorMessage={error.name}
-                    onChangeText={setName}/>
-                <Input
-                    placeholder='Email'
-                    errorMessage={error.email}
-                    onChangeText={setEmail}/>
-                <Input
-                    placeholder='Alamat'
-                    errorMessage={error.address}
-                    onChangeText={setAddress}/>
-                <Input
-                    placeholder='No. HP'
-                    errorMessage={error.phoneNumber}
-                    onChangeText={setPhoneNumber}/>
-                <Input
-                    placeholder='Hobi'
-                    errorMessage={error.hobby}
-                    onChangeText={setHobby}/>
-                <Input
-                    placeholder='Makanan Favorit'
-                    errorMessage={error.favFood}
-                    onChangeText={setFavFood}/>
-                <Input
-                    placeholder='Minuman Favorit'
-                    errorMessage={error.favBeverage}
-                    onChangeText={setFavBeverage}/>
-                <Input
-                    placeholder='Buku Favorit'
-                    errorMessage={error.favBook}
-                    onChangeText={setFavBook}/>
-                <Input
-                    placeholder='Moto Hidup'
-                    errorMessage={error.motto}
-                    onChangeText={setMotto}/>
-                <Input
-                    placeholder='Password'
-                    errorMessage={error.password}
-                    onChangeText={setPassword}/>
-                <Input
-                    placeholder='Konfirmasi Password'
-                    errorMessage={error.confirmationPassword}
-                    onChangeText={setConfirmationPassword}/>
-                <Button title={'Daftar'} onPress={onSubmit}/>
+                <Controller
+                    name={'name'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Nama'
+                            errorMessage={errors.name?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'email'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Email'
+                            errorMessage={errors.email?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'address'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Alamat'
+                            errorMessage={errors.address?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'phoneNumber'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='No. HP'
+                            errorMessage={errors.phoneNumber?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'hobby'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Hobi'
+                            errorMessage={errors.hobby?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'favFood'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Makanan Favorit'
+                            errorMessage={errors.favFood?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'favBeverage'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Minuman Favorit'
+                            errorMessage={errors.favBeverage?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'favBook'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Buku Favorit'
+                            errorMessage={errors.favBook?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'motto'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Moto Hidup'
+                            errorMessage={errors.motto?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'password'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Password'
+                            errorMessage={errors.password?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Controller
+                    name={'confirmationPassword'}
+                    defaultValue={''}
+                    control={control}
+                    render={(onChange) => (
+                        <Input
+                            placeholder='Konfirmasi Password'
+                            errorMessage={errors.confirmationPassword?.message}
+                            onChangeText={val => onChange(val)}/>
+                    )}/>
+                <Button title={'Daftar'} onPress={handleSubmit(onSubmit)}/>
             </View>
         </ScrollView>
     );
